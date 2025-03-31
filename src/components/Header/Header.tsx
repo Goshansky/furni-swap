fbd59c1f3307a774c84b54c47f84711d5c4a58c9
Гошанский <mc.goshanskyi@yandex.ru>
2025-03-31 16:25:41 +0300

    app: Правка Хэдера

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,11 +12,11 @@ const Header = () => {
                     <h1 className={styles.logo}>Furni-Swap</h1>
                 </Link>
 
-                <h2 className={styles.tagline}>Покупайте и продавайте с комфортом!</h2>
+                <h2 className={styles.logo}>Покупайте и продавайте с комфортом!</h2>
                 <nav className={styles.nav}>
                     <Link to="/catalog" className={styles.navButton}>Каталог</Link>
                     <Link to="/sell" className={styles.navButton}>Продать</Link>
-                    <Link to="/profile" className={styles.navButton}>Профиль</Link>
+                    <Link to="/register" className={styles.navButton}>Профиль</Link>
                 </nav>
             </div>
         </header>
